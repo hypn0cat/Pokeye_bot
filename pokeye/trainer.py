78b8a02e37d424adc286fab7c4bab52fd8ae331f
leo <thehypnocat@gmail.com>
2022-10-15 17:47:31 +0300

    Fixes

--- pokeye/trainer.py
+++ pokeye/trainer.py
@@ -164,13 +164,13 @@ model.add(Dense(len(imbalanced), activation='softmax'))
 # model.summary()
 
 # Сохраняем лучшую модель
-checkpoint = ModelCheckpoint(Path('working/best_model.hdf5'), verbose=1, monitor='val_accuracy', save_best_only=True)
+# checkpoint = ModelCheckpoint(Path('working/best_model.hdf5'), verbose=1, monitor='val_accuracy', save_best_only=True)
 
 # В качестве функции потерь используем категориальную перекрестную энтропию
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=32), epochs=100,
-                              validation_data=(X_test, y_test),
-                              steps_per_epoch=len(X_train) // 32, callbacks=[checkpoint])
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=32), epochs=100,
+#                               validation_data=(X_test, y_test),
+#                               steps_per_epoch=len(X_train) // 32, callbacks=[checkpoint])
 
 # Plot learning curves
 # fig = plt.figure(figsize=(17, 4))
@@ -198,65 +198,62 @@ model.save(Path('working/model.hdf5'))
 
 
 
+# Датафрейм изображений
+mewtwo = ['https://files.cults3d.com/uploaders/17560495/illustration-file/8e1f6705-9f7e-4721-999a-35d1043ab419/meewtwo.jpg',
+          'https://images.immediate.co.uk/production/volatile/sites/3/2022/06/Pokemon-Go-Mewtwo-counters-af97a91.jpg',
+          'https://cdn.vox-cdn.com/thumbor/W-3KoS_ImZ6ZsL4zujBCknYCpcg=/0x0:1750x941/1400x1400/filters:focal(735x331:1015x611):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/53111665/Mewtwo_M01.0.0.png']
 
-# mewtwo = ['https://cdn.bulbagarden.net/upload/thumb/7/78/150Mewtwo.png/250px-150Mewtwo.png',
-#           'https://cdn.vox-cdn.com/thumbor/sZPPvUyKyF97UEU-nNtVnC3LpF8=/0x0:1750x941/1200x800/filters:focal(878x316:1158x596)/cdn.vox-cdn.com/uploads/chorus_image/image/63823444/original.0.jpg',
-#           'https://images-na.ssl-images-amazon.com/images/I/61j5ozFjJ0L._SL1024_.jpg']
-#
-# pikachu = [
-#     'https://lh3.googleusercontent.com/proxy/DrjDlKlu9YonKbj3iNCJNJ3DGqzy9GjeXXSUv-TcVV4UN9PMCAM5yIkGLPG7wYo3UeA4sq5OmUWM8M6K5hy2KOAhf8SOL3zPH3axb2Xo3HX2XTU8M2xW4X6lVg=w720-h405-rw',
-#     'https://giantbomb1.cbsistatic.com/uploads/scale_medium/0/6087/2437349-pikachu.png',
-#     'https://johnlewis.scene7.com/is/image/JohnLewis/237525467']
-#
-# charmander = ['https://img.pokemondb.net/artwork/large/charmander.jpg',
-#               'https://www.pokemoncenter.com/wcsstore/PokemonCatalogAssetStore/images/catalog/products/P5073/701-03990/P5073_701-03990_01.jpg',
-#               'https://static.posters.cz/image/750/%D0%A7%D0%B0%D1%88%D0%BA%D0%B0/pokemon-charmander-glow-i72513.jpg']
-#
-# bulbasaur = ['https://img.pokemondb.net/artwork/large/bulbasaur.jpg',
-#              'https://ae01.alicdn.com/kf/HTB1aWullxSYBuNjSsphq6zGvVXaR/Big-Size-55-CM-Plush-Toy-Squirtle-Bulbasaur-Charmander-Toy-Sleeping-Pillow-Doll-For-Kid-Birthday.jpg',
-#              'https://cdn.bulbagarden.net/upload/thumb/f/f7/Bulbasaur_Detective_Pikachu.jpg/250px-Bulbasaur_Detective_Pikachu.jpg']
-#
-# squirtle = ['https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
-#             'https://cdn.vox-cdn.com/thumbor/l4cKX7ZWargjs-zlxOSW2WZVgfI=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/61498573/jbareham_180925_ply0802_0030.1537570476.jpg',
-#             'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidthier%2Ffiles%2F2018%2F07%2FSquirtle_Squad.jpg']
-#
-# test_df = [mewtwo, pikachu, charmander, bulbasaur, squirtle]
-#
-# # Lists to store our future data
-# val_x = []
-# val_y = []
-#
-# for i, urls in enumerate(test_df):
-#     for url in urls:
-#         r = requests.get(url, stream=True).raw
-#         image = np.asarray(bytearray(r.read()), dtype="uint8")
-#         image = cv.imdecode(image, cv.IMREAD_COLOR)
-#         val_x.append(image)
-#         val_y.append(i)
-#
-# # plt.imshow(image[:, :, ::-1])
-#
-# rows = 5
-# cols = 3
-#
-# fig = plt.figure(figsize=(25, 25))
-#
-# for i, j in enumerate(zip(val_x, val_y)):  # i - for subplots
-#     orig = j[0]  # Original, not resized image
-#     label = j[1]  # Label for that image
-#
-#     image = cv.resize(orig, (96, 96))  # Resizing image to (96, 96)
-#     image = image.reshape(-1, 96, 96, 3) / 255.0  # Reshape and scale resized image
-#     preds = model.predict(image)  # Predicting image
-#     pred_class = np.argmax(preds)  # Defining predicted class
-#
-#     true_label = f'True class: {imbalanced[label]}'
-#     pred_label = f'Predicted: {imbalanced[pred_class]} {round(preds[0][pred_class] * 100, 2)}%'
-#
-#     fig.add_subplot(rows, cols, i + 1)
-#     plt.imshow(orig[:, :, ::-1])
-#     plt.title(f'{true_label}\n{pred_label}')
-#     plt.axis('off')
-#
-# plt.tight_layout()
-# plt.show()
+pikachu = ['https://lh3.googleusercontent.com/proxy/DrjDlKlu9YonKbj3iNCJNJ3DGqzy9GjeXXSUv-TcVV4UN9PMCAM5yIkGLPG7wYo3UeA4sq5OmUWM8M6K5hy2KOAhf8SOL3zPH3axb2Xo3HX2XTU8M2xW4X6lVg=w720-h405-rw',
+           'https://trashbox.ru/files/626440_8c7af7/pnwrpse.png',
+           'https://johnlewis.scene7.com/is/image/JohnLewis/237525467']
+
+charmander = ['https://img.pokemondb.net/artwork/large/charmander.jpg',
+              'https://www.pokemoncenter.com/wcsstore/PokemonCatalogAssetStore/images/catalog/products/P5073/701-03990/P5073_701-03990_01.jpg',
+              'https://static.posters.cz/image/750/%D0%A7%D0%B0%D1%88%D0%BA%D0%B0/pokemon-charmander-glow-i72513.jpg']
+
+bulbasaur = ['https://img.pokemondb.net/artwork/large/bulbasaur.jpg',
+             'https://ae01.alicdn.com/kf/HTB1aWullxSYBuNjSsphq6zGvVXaR/Big-Size-55-CM-Plush-Toy-Squirtle-Bulbasaur-Charmander-Toy-Sleeping-Pillow-Doll-For-Kid-Birthday.jpg',
+             'https://archives.bulbagarden.net/media/upload/f/f7/Bulbasaur_Detective_Pikachu.jpg']
+
+squirtle = ['https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+            'https://cdn.vox-cdn.com/thumbor/l4cKX7ZWargjs-zlxOSW2WZVgfI=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/61498573/jbareham_180925_ply0802_0030.1537570476.jpg',
+            'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidthier%2Ffiles%2F2018%2F07%2FSquirtle_Squad.jpg']
+
+test_df = [mewtwo, pikachu, charmander, bulbasaur, squirtle]
+
+# Списки для хранения данных
+val_x = []
+val_y = []
+
+for i, urls in enumerate(test_df):
+    for url in urls:
+        r = requests.get(url, stream=True).raw
+        image = np.asarray(bytearray(r.read()), dtype="uint8")
+        image = cv.imdecode(image, cv.IMREAD_COLOR)
+        val_x.append(image)
+        val_y.append(i)
+
+rows = 5
+cols = 3
+
+fig = plt.figure(figsize=(25, 25))
+
+for i, j in enumerate(zip(val_x, val_y)):  #
+    orig = j[0]  # Оригинал изображения
+    label = j[1]  # Название класса
+
+    image = cv.resize(orig, (96, 96))  # Сжимаем до (96, 96)
+    image = image.reshape(-1, 96, 96, 3) / 255.0  # Изменяем форму и масштаб
+    preds = model.predict(image)  # Изображение для распознавания
+    pred_class = np.argmax(preds)  # Определение класса
+
+    true_label = f'Истинный класс: {imbalanced[label]}'
+    pred_label = f'Предсказанный: {imbalanced[pred_class]} {round(preds[0][pred_class] * 100, 2)}%'
+
+    fig.add_subplot(rows, cols, i + 1)
+    plt.imshow(orig[:, :, ::-1])
+    plt.title(f'{true_label}\n{pred_label}', fontsize=14)
+    plt.axis('off')
+
+plt.tight_layout()
+plt.show()
